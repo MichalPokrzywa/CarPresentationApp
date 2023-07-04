e5b8fecaff6b33f0e8e95756473c66552f5ba7d2
MichalPokrzywa <michpok467@student.polsl.pl>
2023-07-04 16:44:40 +0200

    Added new views to menu
    Added ButtonsManager and ViewGroupManager to segregate current views

--- Assets/Scripts/UI/ButtonsManager.cs
+++ Assets/Scripts/UI/ButtonsManager.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class ButtonsManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+	List<Button> buttons = new List<Button>();
+	
+	int currentButton;
+	// Start is called before the first frame update
+    void Start() {
+	    currentButton = 0;
+	    foreach (Transform child in transform) {
+		    buttons.Add(child.GetComponent<Button>());
+		    child.GetComponent<Button>().onClick.AddListener(ChangeView);
+
+		}
+		buttons[0].Select();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void ChangeView() {
+	    for (int i = 0; i < buttons.Count; i++) {
+		    if (buttons[i].gameObject == EventSystem.current.currentSelectedGameObject) {
+				currentButton = i;
+				ViewGroupManager views = ViewGroupManager.instance;
+				views.ChangeView(i);
+				break;
+		    }
+	    }
     }
+
 }
---
+++ Assets/Scripts/UI/ViewGroupManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewGroupManager : MonoBehaviour
+{
+	private static ViewGroupManager _instance;
+	public static ViewGroupManager instance => _instance;
+
+	List<GameObject> views = new List<GameObject>();
+	int currentView;
+	void Awake() {
+		if (_instance != null && _instance != this) {
+			Destroy(gameObject);
+		}
+		else {
+			_instance = this;
+		}
+	}
+
+	void Start() {
+		currentView = 0;
+		foreach (Transform child in transform) {
+			views.Add(child.gameObject);
+		}
+		views[0].SetActive(true);
+	}
+
+	public void ChangeView(int newView) {
+		views[currentView].SetActive(false);
+		views[newView].SetActive(true);
+		currentView = newView;
+	}
+}
